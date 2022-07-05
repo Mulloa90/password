40f0e43b6d731d2147a96eb4d87c27d94688d13e
mulloa90 <marioulloa@Marios-iMac.attlocal.net>
2022-07-04 23:10:33 -0700

    update variables

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Assignment code here
+var characterLength = 8;
+var choose = [];
+
+var char = ['=','-',')','(','*','&','^','%','$','#','@','!','+','_','/','?','<','>'];
+var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var num = ['0','1','2','3','4','5','6','7','8','9'];
+
+
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +27,10 @@ function writePassword() {
 
 }
 
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
