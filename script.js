05b2aba5294c50a8f0cea2d66bb15ad785d7be7b
mulloa90 <marioulloa@Marios-iMac.attlocal.net>
2022-07-05 16:20:08 -0700

    updated functions

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 // Assignment code here
 var characterLength = 8;
-var choose = [];
+var chooseArray = [];
 
-var char = ['=','-',')','(','*','&','^','%','$','#','@','!','+','_','/','?','<','>'];
-var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var num = ['0','1','2','3','4','5','6','7','8','9'];
-
-
-
-
+var characterArray = ['=','-',')','(','*','&','^','%','$','#','@','!','+','_','/','?','<','>'];
+var lowerCaseArray =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArray =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArray = ['0','1','2','3','4','5','6','7','8','9'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
+function writePassword() {
+  var passPrompts = getPrompts(); //boolean
+
+  if (passPrompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
+
+// I get password based on prompt answers
+function generatePassword(){
+ var password = "";
+}
+// used parseInt to return character 
+function getPrompts(){
+    chooseArray = [];
+    characterLength = parseInt(prompt("Choose password character length (8-128 charaters)"));
+    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Type selection using numeric keys only between 8-128")
+    return false;
+    }
+
+    if (confirm("Would you like to include lower case letters in your password")){
+    chooseArray = chooseArray.concat(lowerCaseArray);
+    }
+
+    if (confirm("Would you like to include Upper case letters in your password")){
+    chooseArray = chooseArray.concat(upperCaseArray);
+    }
+
+    if (confirm("Would you like to include Numbers in your password")){
+    chooseArray = chooseArray.concat(numberArray);
+    }
+    return true;
+}
